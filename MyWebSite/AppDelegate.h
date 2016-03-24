8ac80d934d333985a26d14372e8aa0d794650700
DW-Under-Sunshine <DogWang@call-me-dogbro.local>
2016-03-24 12:57:44 +0800

    Initial Commit

---
+++ MyWebSite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyWebSite
+//
+//  Created by DW on 16/3/24.
+//  Copyright © 2016年 DW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyWebSite/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyWebSite
+//
+//  Created by DW on 16/3/24.
+//  Copyright © 2016年 DW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
